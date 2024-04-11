7c87aba6a7b9d382708ccaf00b600a797037078f
YU-LUN-LEE <95911604+Alanlee0323@users.noreply.github.com>
2024-04-11 17:56:11 +0800

    update

--- Function_pack.py
+++ Function_pack.py
@@ -16,21 +16,28 @@ def calculate_and_plot_correlation(file_path, reference_col_index, savefig_path)
     reference_column = data.iloc[:, reference_col_index].astype(int)
     print("The reference column is :" ,data.columns[reference_col_index])
 
+
+    #轉換HomePlanet
+    dummies = pd.get_dummies(data["HomePlanet"]).astype(int)
+    data_with_dummies = pd.concat([data, dummies], axis = 1)
+    print(data_with_dummies)
+
+
     # 將布林值列轉換為整數
     bool_columns = ['CryoSleep', 'VIP', 'Transported']  # 需要轉換的布林列表
     for col in bool_columns:
         if col in data.columns:
-            data[col] = data[col].astype(int)
+            data_with_dummies[col] = data_with_dummies[col].astype(int)
 
     print("Finished Data",data.head())
 
     # 計算其他列相對於參考列的Pearson相關性
     correlations = []
     columns_checked = []
-    for column in data.columns:
-        if column != data.columns[reference_col_index] and data[column].dtype in ['float64', 'int64', 'int32']:
+    for column in data_with_dummies.columns:
+        if column != data_with_dummies.columns[reference_col_index] and data_with_dummies[column].dtype in ['float64', 'int64', 'int32']:
             columns_checked.append(column)
-            current_column = data[column].astype(float)  # 確保列是數值類型
+            current_column = data_with_dummies[column].astype(float)  # 確保列是數值類型
             correlation = reference_column.corr(current_column)
             correlations.append(correlation)
 
@@ -42,10 +49,11 @@ def calculate_and_plot_correlation(file_path, reference_col_index, savefig_path)
 
     plt.figure(figsize=(10, 6))
     sns.barplot(x='Correlation', y='Column', data=correlation_data, palette='viridis')
-    plt.title('Pearson Correlation with Reference Column (Column {})'.format(reference_col_index))
+    reference_col_name = data.columns[reference_col_index]
+    plt.title('Pearson Correlation with Reference Column => {}'.format(reference_col_name))
     plt.xlabel('Pearson Correlation Coefficient')
     plt.ylabel('Column Name')
     plt.savefig(savefig_path)
     plt.show()
 
-calculate_and_plot_correlation('C:\\Users\\alana\\Downloads\\train.csv', 9, r'C:\Users\alana\Dropbox\Datamining_class\result.png')
+calculate_and_plot_correlation(r'C:\Users\alana\Dropbox\Datamining_class\train.csv', 13, r'C:\Users\alana\Dropbox\Datamining_class\result.png')
